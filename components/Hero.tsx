2f9cab680070c9ff2b87c79f9b4439369817e55e
Omar Nassar <omar.portero.nassar1@gmail.com>
2025-03-01 20:39:18 +0100

    No message

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -152,17 +152,6 @@ const Hero = () => {
         </div>
       </div>
       
-      {/* Gradient divider */}
-      <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-white dark:from-gray-950 to-transparent">
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-center">
-          <div className="text-sm text-gray-400 dark:text-gray-500 flex flex-col items-center animate-pulse">
-            <span>{t('hero.scrollDown')}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-            </svg>
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
